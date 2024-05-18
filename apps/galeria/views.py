0a2ce380253b1079cb6d7d228a9598a899b6bf27
roniwellington <roniwellington@gmail.com>
2024-05-17 23:52:16 -0300

    projeto-estudo-concluido

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 
@@ -25,4 +26,44 @@ def buscar(request):
         nome_a_buscar = request.GET['buscar']
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
-    return render(request, "galeria/buscar.html", {"cards": fotografias})
+    return render(request, "galeria/index.html", {"cards": fotografias})
+
+
+def nova_imagem(request):
+        if not request.user.is_authenticated:
+            messages.error(request, "Usuário não logado")
+            return redirect('login')
+
+        form = FotografiaForms
+        if request.method == 'POST':
+            form = FotografiaForms(request.POST, request.FILES)
+            if form.is_valid():
+                 form.save()
+                 messages.success(request, 'Nova fotografia cadastrada!')
+                 return redirect('index')
+
+        return render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request, foto_id):
+        fotografia = Fotografia.objects.get(id=foto_id)
+        form = FotografiaForms(instance=fotografia)
+
+        if request.method == 'POST':
+             form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+             if form.is_valid():
+                form.save()
+                messages.success(request, 'Fotografia editada com sucesso!')
+                return redirect('index')
+
+        return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
+
+def deletar_imagem(request, foto_id):
+        fotografia = Fotografia.objects.get(id=foto_id)
+        fotografia.delete()
+        messages.success(request, 'Deleção feita com sucesso!')
+        return redirect('index')
+
+def filtro(request, categoria):
+     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria)
+     return render(request, 'galeria/index.html', {"cards": fotografias})
+
